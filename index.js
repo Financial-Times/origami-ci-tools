204900dd97dafdcfd8fb5d3e8d1b3a959114cbcc
chee <chee@snoot.club>
2019-10-02 13:48:48 +0100

    Fix style of index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 // This lets us use import/export in the rest of the program
-require = require("esm")(module)
-module.exports = require("./main.js")
+require = require('esm')(module);
+module.exports = require('./main.js');
