95a810db35d2bcafc5025c8e13d42e996bca31a1
Jake Champion <jakechampion.jake2@googlemail.com>
2019-12-10 17:54:14 +0000

    run occ first to get the correct package.json.name for npm info to then use to get the correct versions from npmjs registry

--- commands/release.js
+++ commands/release.js
@@ -37,8 +37,10 @@ export async function command() {
 	*/
 	const newVersion = coerce(env.version);
 	const newVersionIsNotPrerelease = newVersion.prerelease.length === 0;
+	
+	await exec('occ', '--name', env.name, "0.0.0");
 
-	const versions = await exec('npm', 'info', env.name, 'versions', '--json');
+	const versions = await exec('npm', 'info', '.', 'versions', '--json');
 
 	const stableVersions = versions.filter(version => {
 		const v = coerce(version);
@@ -49,7 +51,7 @@ export async function command() {
 		return gt(newVersion, version);
 	});
 
-	await exec('occ', '--name', env.name, env.version);
+	await exec('npm', 'version', env.version);
 
 	if (newVersionIsNotPrerelease && newVersionIsLargestVersion) {
 		await exec('npm', 'publish', '--access', 'public');
