70b3f8fa70699a3080765c229ba900cba776505e
notlee <lee.moody@ft.com>
2020-02-12 14:58:41 +0000

    Fix npm publish for new components.

--- commands/release.js
+++ commands/release.js
@@ -1,9 +1,11 @@
-import {promises as fs} from 'fs';
-import {homedir} from 'os';
-import {resolve as resolvePath} from 'path';
-import {gt, coerce} from 'semver';
+import { promises as fs } from 'fs';
+import { homedir } from 'os';
+import { resolve as resolvePath } from 'path';
+import { gt, coerce } from 'semver';
 
+import execa from 'execa';
 import exec from '../lib/exec';
+import { getExecEnv } from '../lib/exec';
 import env from '../lib/env';
 
 export let description = 'run release commands';
@@ -25,14 +27,14 @@ export async function command() {
 
 	/*
 		Publishing to npm without `--tag` being set will make that version have the dist-tag `latest`.
-		When someone does an install of the package without declaring a version or dist-tag then the 
+		When someone does an install of the package without declaring a version or dist-tag then the
 		`latest` dist-tag is used. This is a problem if an Origami component has a fixed/feature backported
 		to an old version and that old version is published, because it will then be tagged as the `latest`
 		version, which means users would not get the version they expect when they run `npm install o-component`.
 
 		The code below attempts to solve the above issue by checking that the version being published is not
 		 a prerelease and is the largest version compared to all previously published version. If the version
-		 being published is either a prerelease or not the largest version then we tag the release with the 
+		 being published is either a prerelease or not the largest version then we tag the release with the
 		 version to ensure that it does not get tagged with `latest`.
 	*/
 	const newVersion = coerce(env.version);
@@ -40,7 +42,18 @@ export async function command() {
 
 	await exec('occ', '--name', env.name, '0.0.0');
 
-	const versions = await exec('npm', 'info', '.', 'versions', '--json');
+	let versions = [];
+
+	try {
+		versions = await execa('npm', ['info', '.', 'versions', '--json'], {
+			env: getExecEnv()
+		});
+	} catch (error) {
+		// do not error if the component does not yet exist in npm
+		if (!(typeof error.stderr === 'string' && error.stderr.includes('E404'))) {
+			throw error;
+		}
+	}
 
 	const stableVersions = versions.filter(version => {
 		const v = coerce(version);
