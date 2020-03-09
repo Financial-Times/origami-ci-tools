eb887968403ad9a810e5f02b4a0297ba38f62908
chee <chee@snoot.club>
2020-03-09 14:36:02 +0000

    Test the to-be-published npm artifact during branch build

--- commands/branch.js
+++ commands/branch.js
@@ -1,6 +1,15 @@
 import exec from '../lib/exec';
 import env from '../lib/env';
 import bundleSize from '../lib/bundle-size-pr-comment.js';
+import {promises as fs, existsSync} from 'fs';
+import tar from 'tar';
+import {promisify} from 'util';
+import ncp from 'ncp';
+
+// copy 16 files at once
+ncp.limit = 16;
+
+let copyRecursively = promisify(ncp);
 
 export let description = 'run branch commands';
 
@@ -10,14 +19,34 @@ export let globalDependencies = [
 	'@financial-times/origami-bundle-size-cli',
 ];
 
+let getShortId = () =>
+	Math.random()
+		.toString(36)
+		.slice(2);
+
 export async function command() {
+	let temporaryVersion = `0.0.0`;
+	let tarballFileName = `financial-times-${env.name}-${temporaryVersion}.tgz`;
+	let buildDir = 'occ-build-' + getShortId();
 	await exec('obt', 'install');
 	await exec('obt', 'demo', '--demo-filter', 'pa11y', '--suppress-errors');
 	await exec('obt', 'verify');
 	await exec('obt', 'test');
 	await exec('git', 'clean', '-fxd');
-	await exec('occ', '--name', env.name, '0.0.0');
+	await exec('occ', '--name', env.name, temporaryVersion);
+	await exec('npm', 'pack');
+	await fs.mkdir(buildDir);
+	await tar.extract({
+		file: tarballFileName,
+		strip: 1,
+		cwd: buildDir,
+	});
+	process.chdir(buildDir);
 	await exec('obt', 'install', '--ignore-bower');
+	if (existsSync('../test')) {
+		await copyRecursively('../test', './test');
+	}
 	await exec('obt', 'test', '--ignore-bower');
+	process.chdir('..');
 	await bundleSize();
 }
