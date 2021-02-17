14709d164a688f80014ffc3edf1c65d4bca29f3d
notlee <notlee@users.noreply.github.com>
2021-02-17 16:21:13 +0000

    Remove bundle size comparison logic
    `origami-bundle-size-cli` can no longer support bundle size
    comparisons for commit refs. We intend to solve the problem of
    ensuring component bundle sizes are kept in check some other way
    in the future

--- commands/branch.js
+++ commands/branch.js
@@ -1,16 +1,13 @@
 import exec from '../lib/exec';
-import bundleSize from '../lib/bundle-size-pr-comment.js';
 
 export let description = 'run branch commands';
 
 export let globalDependencies = [
-	'origami-build-tools',
-	'@financial-times/origami-bundle-size-cli',
+	'origami-build-tools'
 ];
 
 export async function command() {
 	await exec('obt', 'install');
 	await exec('obt', 'verify');
 	await exec('obt', 'test');
-	await bundleSize();
 }
